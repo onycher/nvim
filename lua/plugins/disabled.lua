2be22a057b9bd4bd85b068286344f86b00a110f7
Sebastian Onycher <sebastian.onycher@flir.se>
2025-08-19 13:07:49 +0200

    Set lsp

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,7 @@
+return {
+  { "stevearc/conform.nvim",          enabled = false },
+  { "folke/flash.nvim",               enabled = false },
+  { "folke/trouble.nvim",             enabled = false },
+  { "mason-org/mason.nvim",           enabled = false },
+  { "mason-org/mason-lspconfig.nvim", enabled = false },
+}
---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        pylsp = {},
+        robotcode = {},
+        cucumber_language_server = {
+          settings = {
+            cucumber = {
+              glue = { "steps/**/*.py", }, 
+            },
+          },
+        },
+      },
+    },
+  },
+
+}
